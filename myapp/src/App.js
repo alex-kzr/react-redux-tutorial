ba0af4efdca85b96375b7eccf67ff3c13e589bd9
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-21 11:05:12 +1000

    Add nesting component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninja';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
+        <Ninjas />
       </div>
     );
   }  
